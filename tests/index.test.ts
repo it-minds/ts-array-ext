eceec8be0442274541c90b1840ed5c914ab3b554
Martin Johannesson <mjh@it-minds.dk>
2020-09-29 14:29:19 +0200

    Added some description to the shuffle test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -93,7 +93,7 @@ describe("sort", function () {
 });
 
 describe("shuffle", () => {
-  it("Shuffle Then sort", function (done) {
+  it("Shuffle 1 mio times and expect results to be close to average", function (done) {
     this.timeout(5000);
     let roll = 0;
     let counts: { [k: string]: number } = {};
@@ -104,10 +104,15 @@ describe("shuffle", () => {
 
     while (roll < 1000000) {
       roll++;
+      //increase the count of the index value of the newly shuffled array.
       counts[myArr.shuffle()[0].index]++;
     }
 
+    //Check each index value. If it is within the error margin, that index was shuffled randomly.
     const result = Object.keys(counts).filter(
+      // numbers are derived from the array length (17) with an error margin of 0.2% (2000)
+      // lower: (1,000,000 / 17) round to lowest 2000th = 58000
+      // upper: (1,000,000 / 17) round to highest 2000th = 60000
       x => counts[x] > 58000 && counts[x] < 60000
     ).length;
 
