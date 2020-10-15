0ae96d3c5fabf50356008df159bc5871170848c3
Jeppe Trip Kofoed <jtk@it-minds.dk>
2020-10-15 14:43:07 +0200

    added an example to the documentation comment.

--- src/types.ts
+++ src/types.ts
@@ -235,6 +235,14 @@ interface Array<T> {
 
   /**
    * Produce an array with distinct elements based on a single comparable attribute.
+   * Example
+   * ```typescript
+   * const arr = ["banana", "strawberry", "lemon", "banana", "strawberry", "lemon", "banana", "strawberry", "lemon"];
+   * const res = arr.distinct();
+   * 
+   * const arr = [{ id: 0, fruit: "banana" }, { id: 1, fruit: "strawberry" }, { id: 2, fruit: "lemon" }];
+   * const res = arr.distinct(x => x.fruit);
+   * ```
    */
   distinct(
     callbackfn?: (value: T) => string | number | symbol,
