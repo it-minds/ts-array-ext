83a177f546b93c7f3a230ac7bdf455302b16a7d3
Jeppe Trip Kofoed <jtk@it-minds.dk>
2020-10-16 11:11:50 +0200

    Moved the typedefinition and implementation of chunkByCount into its own file.

---
+++ src/chunkByCount.ts
@@ -0,0 +1,33 @@
+import { Exception_OutOfBounds } from "./customErrors";
+
+const chunkByCount: <T>(chunkCount: number, forceFairness?: boolean, thisArg?: any[]) => T[][] = function (chunkCount, forceFairness = false, thisArg = this) {
+    if (chunkCount <= 0) throw new Exception_OutOfBounds();
+
+    const result: any[][] = [];
+    for (let i = chunkCount; i > 0; i--) {
+        result.push([]);
+    }
+
+    const fairness = forceFairness ? thisArg.length % chunkCount : 0;
+    thisArg.slice(0, thisArg.length - fairness).forEach((ele, i) => {
+        const group = i % chunkCount;
+        result[group].push(ele);
+    });
+
+    return result;
+};
+
+/**
+ * Split an array into X amount of chunks.
+ *
+ * Optional force fairness that strips the array down so `% X === 0`
+ *
+ * Example:
+ * ```typescript
+ * const chunks = myArr.chunkByCount(5);
+ * //If the arr contains 21 elements, the 21st element will be part of the first array.
+ * const chunks = myArr.chunkByCount(5, true);
+ * //If the arr contains 21 elements, the 21st element wont be part of the result here.
+ * ```
+ */
+Array.prototype.chunkByCount = chunkByCount;
