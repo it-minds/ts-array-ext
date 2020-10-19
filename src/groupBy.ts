049104a9d36e62f63c65b641d57ec36b3fbf4093
Jeppe Trip Kofoed <jtk@it-minds.dk>
2020-10-19 09:16:26 +0200

    updated a link.

--- src/groupBy.ts
+++ src/groupBy.ts
@@ -1,9 +1,9 @@
 /**
  * This file does not contain a top level import or export and is by the typescript definition thus not a module, and instead treated like a script
- * whose content are available in the global scope. The 'declare global' statement seen in some other files (such as average.ts) isn't required (and cannot be 
+ * whose content are available in the global scope. The 'declare global' statement seen in some other files (such as average.ts) isn't required (and cannot be
  * placed here at all) since we by default work within the global scope.
  * https://www.typescriptlang.org/docs/handbook/modules.html
- * https://www.typescriptlang.org/docs/handbook/declaration-files/by-example.html
+ * https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-modifying-module-d-ts.html
  */
 interface Array<T> {
   /**
