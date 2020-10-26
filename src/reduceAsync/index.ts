f2e99fa5390fe029364b1a79068b57b82abf27f9
Jeppe Trip Kofoed <jtk@it-minds.dk>
2020-10-26 11:05:45 +0100

    Added export and global declarations.

--- src/reduceAsync/index.ts
+++ src/reduceAsync/index.ts
@@ -1,25 +1,33 @@
-interface Array<T> {
-  /**
-   * Avoid having to fiddle with promise accumulator and resolution in a .reduce.
-   *
-   * Example: (a map would be better here - sue me)
-   * ```typescript
-   * const resultProm = myArr.reduceAsync<ExtendedScore[]>(async (acc, cur) => {
-   *   const addOnAttr = await new Promise<string>(resolve =>
-   *     setTimeout(() => resolve(Date.now().toString(24)), 5)
-   *   );
-   *
-   *   (cur as ExtendedScore).fetchedAttr = addOnAttr;
-   *
-   *   return [...acc, cur as ExtendedScore];
-   * }, []);
-   * ```
-   */
-  reduceAsync<U = T>(
-    callbackfn: (previousValue: U, currentValue: T, currentIndex: number, array: T[]) => Promise<U>,
-    initialVal: U,
-    thisArg?: any[]
-  ): Promise<U>;
+export {};
+declare global {
+  interface Array<T> {
+    /**
+     * Avoid having to fiddle with promise accumulator and resolution in a .reduce.
+     *
+     * Example: (a map would be better here - sue me)
+     * ```typescript
+     * const resultProm = myArr.reduceAsync<ExtendedScore[]>(async (acc, cur) => {
+     *   const addOnAttr = await new Promise<string>(resolve =>
+     *     setTimeout(() => resolve(Date.now().toString(24)), 5)
+     *   );
+     *
+     *   (cur as ExtendedScore).fetchedAttr = addOnAttr;
+     *
+     *   return [...acc, cur as ExtendedScore];
+     * }, []);
+     * ```
+     */
+    reduceAsync<U = T>(
+      callbackfn: (
+        previousValue: U,
+        currentValue: T,
+        currentIndex: number,
+        array: T[]
+      ) => Promise<U>,
+      initialVal: U,
+      thisArg?: any[]
+    ): Promise<U>;
+  }
 }
 
 Array.prototype.reduceAsync = function (callback, initialVal, thisArg = this) {
--- src/sortByAttr/index.ts
+++ src/sortByAttr/index.ts
@@ -1,22 +1,25 @@
-type SortDirection = "ASC" | "DESC";
+export {};
+declare global {
+  type SortDirection = "ASC" | "DESC";
 
-interface Array<T> {
-  /**
-   * Shorthand for sorting an array by a single comparable attribute. Additional options for sorting descending.
-   *
-   * Example
-   * ```typescript
-   * const lowestFirst = myArr.SortByArg(x => x.score);
-   * const lowestFirst = myArr.SortByArg(x => x.score, "ASC");
-   *
-   * const highestFirst = myArr.SortByArg(x => x.score, "DESC");
-   * ```
-   */
-  sortByAttr(
-    callbackfn?: (value: T) => string | number,
-    sortDirection?: SortDirection,
-    thisArg?: any[]
-  ): T[];
+  interface Array<T> {
+    /**
+     * Shorthand for sorting an array by a single comparable attribute. Additional options for sorting descending.
+     *
+     * Example
+     * ```typescript
+     * const lowestFirst = myArr.SortByArg(x => x.score);
+     * const lowestFirst = myArr.SortByArg(x => x.score, "ASC");
+     *
+     * const highestFirst = myArr.SortByArg(x => x.score, "DESC");
+     * ```
+     */
+    sortByAttr(
+      callbackfn?: (value: T) => string | number,
+      sortDirection?: SortDirection,
+      thisArg?: any[]
+    ): T[];
+  }
 }
 
 Array.prototype.sortByAttr = function (func = x => x, sortDirection = "ASC", thisArg = this) {
